58c0c513dbc6acd80a135f9bc50fc013f6aea736
dahami <dahamibandara0730@gmail.com>
2026-06-09 22:05:18 +0530

    Add search and average calculation

--- Main.java
+++ Main.java
@@ -8,9 +8,7 @@ public class Main {
 
     public static void main(String args[]) {
 
-           
-
-    public static void addStudent() {
+           public static void addStudent() {
         System.out.print("Enter ID: ");
         String id = sc.nextLine();
 
@@ -22,7 +20,34 @@ public class Main {
         sc.nextLine();
 
         students.add(new Student(id, name, marks));
-        System.out.println("Student Added!");
+        System.out.println("Student Added");
     }
 
-   }
+    public static void searchStudent() {
+        System.out.print("Enter ID: ");
+        String id = sc.nextLine();
+
+        for (Student s : students) {
+            if (s.getStudentId().equals(id)) {
+                System.out.println(s);
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
+    }
+
+    public static void calculateAverage() {
+        double total = 0;
+
+        for (Student s : students) {
+            total += s.getMarks();
+        }
+
+        if (!students.isEmpty()) {
+            System.out.println("Average = " + (total / students.size()));
+        } else {
+            System.out.println("No students available ");
+        }
+    }
+}
